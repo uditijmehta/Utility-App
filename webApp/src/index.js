b4dd2fa9db98ba454fdb58e60cc88bb0349b952c
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-01 23:18:26 -0500

    adding header

--- webApp/src/index.js
+++ webApp/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+// import App from './App';
+import {Header} from './components/header/header';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
   document.getElementById('root')
 );
