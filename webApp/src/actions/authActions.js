77b4de3e2d6ad0d8f3a43756ba469a8e16fdb2a5
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-06 06:54:21 -0500

    adding actions for auth

--- webApp/src/actions/authActions.js
+++ webApp/src/actions/authActions.js
@@ -2,7 +2,24 @@ import axios from 'axios';
 import setAuthToken from '../utils/setAuthToken';
 import { setAlert } from './alertActions';
 
+// Load User
+export const loadUser = () => async (dispatch) => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+  try {
+    const res = await axios.get('/api/v1/auth/me');
 
+    dispatch({
+      type: 'USER_LOADED',
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: 'AUTH_ERROR',
+    });
+  }
+};
 
 // Register user
 export const register = ({ name, email, password }) => async (dispatch) => {
@@ -33,7 +50,35 @@ export const register = ({ name, email, password }) => async (dispatch) => {
   }
 };
 
+// Login user
+export const login = (email, password) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const body = JSON.stringify({ email, password });
+  try {
+    const res = await axios.post('/api/v1/auth/login', body, config);
 
+    dispatch({
+      type: 'LOGIN_SUCCESS',
+      payload: res.data,
+    });
+
+    dispatch(loadUser());
+  } catch (err) {
+    const errors = err.response.data.errors;
+    if (errors) {
+      errors.forEach((error) => {
+        dispatch(setAlert(error.msg, 'danger'));
+      });
+    }
+    dispatch({
+      type: 'LOGIN_FAIL',
+    });
+  }
+};
 
 // Logout
 export const logout = () => async (dispatch) => {
