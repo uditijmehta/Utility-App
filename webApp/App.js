5182133225423ad4d7e35b282cd3ecd321f76222
Rakesh Baddi <rakeshbaddi@Rakeshs-MacBook-Pro.local>
2021-12-06 00:20:07 -0500

    Created Skeleton of App

---
+++ webApp/App.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+// Router
+import { Route, Switch } from "react-router-dom";
+// redux
+import { useDispatch } from "react-redux";
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
