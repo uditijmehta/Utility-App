07d8c5b6c90237666977b43f0a38886dc329bbcf
Rakesh Baddi <rakeshbaddi@Rakeshs-MacBook-Pro.local>
2021-12-06 18:40:25 -0500

    added styles

--- webApp/src/components/weather/WeatherData.js
+++ webApp/src/components/weather/WeatherData.js
@@ -86,17 +86,53 @@ const StyledWeatherData = styled(motion.div)`
   height: 100%;
   width: 40vw;
   background-color: rgba(255, 255, 255, 0.09);
+  backdrop-filter: blur(8px);
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 4vh 4vw;
+  color: #fff;
   font-size: 1.1rem;
 
+  .search {
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 5rem;
+    height: 4rem;
+    background-color: rgba(8, 8, 8, 0.7);
+    cursor: pointer;
+  }
+  #search {
+    font-size: 2.5rem;
+    color: #1a83ff;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
   .form {
     height: 15%;
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
+    position: relative;
 
     input {
       width: 90%;
       height: 50px;
-      color: #fff;;
+      color: #fff;
+      padding-top: 20px;
+      border: none;
+      outline: 0;
+      background: transparent;
+      border-bottom: 1px solid #ddd;
+      font-size: 1.5rem;
+
+      &:focus {
+        border-bottom-color: #1a83ff;
+      }
     }
 
     label {
@@ -114,15 +150,41 @@ const WeatherDetails = styled(motion.div)`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  @media (max-width: 48rem) {
+    height: 60%;
+    font-size: 1rem;
+  }
 `;
 
 const CurrentWeatherDetails = styled(motion.div)`
   font-size: 1.3rem;
   height: 60%;
+  padding-bottom: 3rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
 
   .weather-holders {
     margin-bottom: 2rem;
-    color: rgba(255, 255, 255, 0.89);
+    h3 {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+      color: rgba(255, 255, 255, 0.89);
+    }
+  }
+  @media (max-width: 48rem) {
+    font-size: 1rem;
+    height: 40%;
+    flex-direction: row;
+    padding-bottom: 1.5rem;
+    .weather-holders {
+      h3 {
+        flex-direction: column;
+        .w-data {
+          margin-top: 0rem;
+        }
+      }
     }
   }
 `;
