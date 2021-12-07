1afe7ef92e405b251a2ffd12bbdb32e1f9dc8f7b
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-07 03:20:38 -0500

    Modification to expense page

--- webApp/src/pages/Expense.js
+++ webApp/src/pages/Expense.js
@@ -7,6 +7,10 @@ import { Header } from "../components/expense/Header";
 import { Balance } from "../components/expense/Balance";
 import { IncomeExpense } from "../components/expense/IncomeExpense";
 import { TransactionList } from "../components/expense/TransactionList";
-import { AddTransaction } from "../components/expense/AddTransaction";\
+import { AddTransaction } from "../components/expense/AddTransaction";
+
+// Actions
+import { updateTransaction } from "../actions/expenseAction";
+import { useSelector, useDispatch } from "react-redux";
 
 export default Expense;
