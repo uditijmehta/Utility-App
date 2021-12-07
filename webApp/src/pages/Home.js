37dd6924de90ce21e4037c9af6e4b388ee752a14
Rakesh Baddi <rakeshbaddi@Rakeshs-MacBook-Pro.local>
2021-12-06 23:59:42 -0500

    added home page

---
+++ webApp/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+
+// Import components
+import Weather from "../components/weather/Weather";
+import News from "../components/news/News";
+
+const Home = () => {
+  return (
+    <div>
+      <Weather />
+      <News />
+    </div>
+  );
+};
+
+export default Home;
