4fd80bed43df37f337c2cdf2b9d4f19b77789cb2
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-07 01:52:07 -0500

    addition of html tags for displaying balance

--- webApp/src/components/expense/Balance.js
+++ webApp/src/components/expense/Balance.js
@@ -5,6 +5,20 @@ import { numberWithCommas } from "../../utils/format";
 // Style
 import styled from "styled-components";
 
+export const Balance = () => {
+    const { transactions } = useSelector((state) => state.expenses);
+    const amounts = transactions.map((transaction) => transaction.amount);
+  
+    const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+  
+    return (
+      <BalanceStyle>
+        <h4>Your Balance</h4>
+        <h1>₹{numberWithCommas(total)}</h1>
+      </BalanceStyle>
+    );
+  };
+  
 const BalanceStyle = styled.div`
   color: #fff;
   h4 {
