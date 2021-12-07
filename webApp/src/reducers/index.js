ff7265e18595066528b01c268d4d87f444e47e4c
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-07 03:51:24 -0500

    Importing all the reducers

---
+++ webApp/src/reducers/index.js
@@ -0,0 +1,9 @@
+import weatherReducer from './weatherReducer';
+import newsReducer from './newsReducer';
+import todoReducer from './todoReducer';
+import expenseReducer from './expenseReducer';
+import alertReducer from './alertReducer';
+import authReducer from './authReducer';
+import { combineReducers } from 'redux';
+
+export default defaultReducer;
