2f18bd46a0d5ff2ad06270a07438b8915002418a
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-07 03:20:11 -0500

    Import components in the expense page

---
+++ webApp/src/pages/Expense.js
@@ -0,0 +1,12 @@
+import React, { useState, useCallback } from "react";
+// Style
+import styled from "styled-components";
+
+// Import components
+import { Header } from "../components/expense/Header";
+import { Balance } from "../components/expense/Balance";
+import { IncomeExpense } from "../components/expense/IncomeExpense";
+import { TransactionList } from "../components/expense/TransactionList";
+import { AddTransaction } from "../components/expense/AddTransaction";\
+
+export default Expense;
