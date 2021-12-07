f1e8c6249031c532110dd98b56d672f49a5a2ca1
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 01:56:04 -0500

    adding notfound page

---
+++ webApp/src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// Style
+import styled from 'styled-components';
+const NotFound = () => {
+  return (
+    <WrapperStyle className='main'>
+      
+    </WrapperStyle>
+  );
+};
+
+export default NotFound;
