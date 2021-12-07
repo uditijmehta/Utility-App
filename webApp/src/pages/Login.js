a1ced6aac4755bda13ffacb9d57ffdca75c9aefb
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 01:43:14 -0500

    adding login page

---
+++ webApp/src/pages/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// Components
+import Login from '../components/auth/Login';
+
+// Style
+import styled from 'styled-components';
+
+const LoginPage = () => {
+  return (
+    <LoginStyle>
+      <Login />
+    </LoginStyle>
+  );
+};
+
+
+export default LoginPage;
