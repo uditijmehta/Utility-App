e964ba7127cfd2819acc5139d210da50d4011d07
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 01:58:02 -0500

    modifying notfound page

--- webApp/src/pages/NotFound.js
+++ webApp/src/pages/NotFound.js
@@ -5,7 +5,14 @@ import styled from 'styled-components';
 const NotFound = () => {
   return (
     <WrapperStyle className='main'>
-      
+      <div class='site'>
+        <div class='sketch'>
+          <div class='bee-sketch red'></div>
+          <div class='bee-sketch blue'></div>
+        </div>
+
+        
+      </div>
     </WrapperStyle>
   );
 };
