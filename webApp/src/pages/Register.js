2f7512f847a4dfc97df81fedd30fd6ec85abd8a3
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 01:48:27 -0500

    modifying register page

--- webApp/src/pages/Register.js
+++ webApp/src/pages/Register.js
@@ -7,8 +7,12 @@ import styled from 'styled-components';
 
 const RegisterPage = () => {
   return (
+    <RegisterStyle>
+      <Register />
+    </RegisterStyle>
   );
 };
 
+const RegisterStyle = styled.div``;
 
 export default RegisterPage;
