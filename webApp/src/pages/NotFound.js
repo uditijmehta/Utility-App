0598bdcbca67b78e7ecb801c662feb8d8198bdc3
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 01:58:46 -0500

    modifying notfound page

--- webApp/src/pages/NotFound.js
+++ webApp/src/pages/NotFound.js
@@ -11,7 +11,13 @@ const NotFound = () => {
           <div class='bee-sketch blue'></div>
         </div>
 
-        
+        <h1>
+          404:
+          <small>Page Not Found</small>
+          <Link to='/'>
+            <button className='btn home-btn'>Home</button>
+          </Link>
+        </h1>
       </div>
     </WrapperStyle>
   );
