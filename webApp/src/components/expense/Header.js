1a682fbee54db992010130faa668e01e1959fbe5
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-07 01:03:02 -0500

    addition of element to expense header

--- webApp/src/components/expense/Header.js
+++ webApp/src/components/expense/Header.js
@@ -3,7 +3,25 @@ import { useSelector } from "react-redux";
 // Style
 import styled from "styled-components";
 
-
+export const Header = () => {
+    const { isAuthenticated, user } = useSelector((state) => state.auth);
+    const Capitalize = (str) => {
+      return str.charAt(0).toUpperCase() + str.slice(1);
+    };
+    return (
+      <HeaderStyle>
+        {isAuthenticated && (
+          <>
+            <h3>Hello {Capitalize(user.data.name)},</h3>
+            <h3>
+              Here's your spending <span>wallet!!</span>
+            </h3>
+          </>
+        )}
+      </HeaderStyle>
+    );
+  };
+  
 const HeaderStyle = styled.div`
   margin-top: 5vh;
   color: #fff;
