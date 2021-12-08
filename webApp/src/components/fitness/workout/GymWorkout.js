4b49362119b5b87a97925f5ff083aa8c2cb7f4b0
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-08 01:08:14 -0500

    Html structure for gym workout page

---
+++ webApp/src/components/fitness/workout/GymWorkout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+export const GymWorkout = () => {
+  return (
+    <Main>
+      <Head>
+        Full-Body <span>Workouts</span> In Gym
+      </Head>
+    </Main>
+  );
+};
