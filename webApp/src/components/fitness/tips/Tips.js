3c89e0c3f987a75793eacd2f65a6c6b879cbd0a4
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-08 01:02:55 -0500

    styling of content in tips page

--- webApp/src/components/fitness/tips/Tips.js
+++ webApp/src/components/fitness/tips/Tips.js
@@ -7,3 +7,30 @@ const Main = styled.div`
 
   background-image: linear-gradient(135deg, #43cbff 10%, #9708cc 100%);
 `;
+
+const Content = styled.div`
+  margin: auto;
+  margin-bottom: 6rem;
+  padding: 3rem;
+  background: rgba(0, 0, 0, 0.8);
+  color: white;
+  border-radius: 40px;
+
+  h1 {
+    margin: 1rem;
+  }
+  span {
+    color: #ff3d3d;
+  }
+  h3 {
+    margin: 0.5rem;
+  }
+  p {
+    padding: 2px;
+  }
+  transition: all 0.75s ease-in-out;
+  &:hover {
+    /* background: rgba(0, 0, 0, 0.8); */
+  }
+`;
+
