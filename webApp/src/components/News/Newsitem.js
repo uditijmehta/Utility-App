5f7779f805b52ef1c6e2e77aaf1f2166f1b462b3
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 19:34:13 -0500

    adding news items

---
+++ webApp/src/components/News/Newsitem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Newsitem = (props)=>{
+        let {title, description, imageUrl, newsUrl, author, date, source} = props;
+        return (
+            <div className="card">
+                
+            </div>
+        )
+}
+
+export default Newsitem
