dfc20be972b4ac8e9fb2e0ff86e1746e47a7830e
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-08 01:09:17 -0500

    html modifications

--- webApp/src/components/fitness/workout/GymWorkout.js
+++ webApp/src/components/fitness/workout/GymWorkout.js
@@ -6,6 +6,65 @@ export const GymWorkout = () => {
       <Head>
         Full-Body <span>Workouts</span> In Gym
       </Head>
+      <Div1>
+        <h3>For Chest And Shoulders</h3>
+        <hr />
+
+        <h5>Bench Press</h5>
+        <br />
+
+        <p>
+          1.Lie on a flat bench, holding a barbell with your hands slightly
+          wider than shoulder-width apart using an overhand grip.
+        </p>
+        <br />
+        <p>
+          2.Brace your core and press your feet into the ground, then lower the
+          bar towards your chest.
+        </p>
+        <br />
+        <p>3.Press it back up to the start.</p>
+        <br />
+        <p>4.Sets 7 Reps 6</p>
+        <br />
+
+        <h5>Lateral raise</h5>
+        <br />
+
+        <p>1.Stand with dumbbells by your sides and palms facing each other</p>
+        <br />
+        <p>
+          2.Lean forward slightly, then raise the weights to the sides, leading
+          with your elbows.
+        </p>
+        <br />
+        <p>3.Slowly lower them back to the start under control.</p>
+        <br />
+        <p>4.Sets 3 Reps 12-15</p>
+        <br />
+
+        <h5>External cable rotation</h5>
+        <br />
+
+        <p>
+          Set the cable at a low height and, using a split stance, retract your
+          shoulder blades.
+        </p>
+        <br />
+        <p>
+          Hold your arm so that your elbow is level with your shoulder and your
+          forearm is parallel to the ground.
+        </p>
+        <br />
+        <p>
+          Pull the cable so that your elbow stays still but your forearm ends in
+          a vertical position.
+        </p>
+        <br />
+        <p>Sets 3 Reps 15</p>
+        <br />
+      </Div1>
+
     </Main>
   );
 };
