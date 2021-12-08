d0bf30ffb3d88ef0a519a0318dd4c72b4eba8fd2
SaiswarnaR <saiswarna.r@northeastern.edu>
2021-12-08 01:02:19 -0500

    tips page of fitness component

---
+++ webApp/src/components/fitness/tips/Tips.js
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const Main = styled.div`
+  padding: 1rem;
+  background-size: cover;
+
+  background-image: linear-gradient(135deg, #43cbff 10%, #9708cc 100%);
+`;
