2002e8e596f21411e2cb6d6e3276593e6a6ab70b
Prasanna Nimbalkar <prasannanimbalkar@Prasannas-MacBook-Pro.local>
2021-12-07 19:44:18 -0500

    modifying news app

--- webApp/src/components/NewsApp.js
+++ webApp/src/components/NewsApp.js
@@ -14,7 +14,22 @@ const NewsApp = ()=>{
   }
     return (
       <>
-       
+        <Router>
+          <Navbar />
+          <LoadingBar
+            color='#f11946'
+            progress={progressBar}
+          />
+          <Switch>
+            <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="newsmonkey" pageSize={pageSize} country="us" category="general" heading="" /></Route>
+            <Route exact path="/top-headlines"><News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="us" category="general" heading=" - Top HeadLines" /></Route>
+            <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="us" category="entertainment" heading=" - Top Entertainment Headlines" /></Route>
+            <Route exact path="/health"><News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="us" category="health" heading=" - Top Health Headlines" /></Route>
+            <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="us" category="science" heading=" - Top Science Headlines" /></Route>
+            <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="us" category="sports" heading=" - Top Sports Headlines" /></Route>
+            <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="us" category="technology" heading=" - Top Technology Headlines" /></Route>
+          </Switch>
+        </Router>
       </>
     )
 }
